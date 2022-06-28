8517374e26777ce6fb27265cd7a9c031d82ac413
Abdullah Khan <abdullahkhan9003@gmail.com>
2022-06-28 20:52:20 +0500

    PORT error is fixed!

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,7 +6,7 @@ const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.u1vlt.mongodb.net/?retryWrites=true&w=majority`;
 
-const PORT = process.env.PORT ? Number(process.env.PORT) : 9000;
+const PORT = process.env.PORT || 9000;
 
 export const config = {
     mongo: {
