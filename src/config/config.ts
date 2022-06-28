dd7c273f726f72eb945b2ad625ceebf0cecf3b47
Abdullah Khan <abdullahkhan9003@gmail.com>
2022-06-28 21:21:29 +0500

    Mongo URI is fixed

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,13 +4,13 @@ dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.u1vlt.mongodb.net/?retryWrites=true&w=majority`;
+const MONGO_URI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@cluster0.u1vlt.mongodb.net/?retryWrites=true&w=majority`;
 
 const PORT = process.env.PORT || 9000;
 
 export const config = {
     mongo: {
-        url: MONGO_URL
+        url: MONGO_URI
     },
     server: {
         port: PORT
